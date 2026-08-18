e5e21cf681f22dda5f10dca1389603dbb06c40ee
Thomas Coughlan <thomascoughlan@Thomass-MacBook-Air.local>
2026-08-18 19:39:50 +0100

    fix: switch astro adapter to cloudflare for production build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'; // Or your specific host adapter (vercel, netlify, etc.)
+import cloudflare from '@astrojs/cloudflare'; 
 
 export default defineConfig({
-  output: 'server', // <-- This is the magic word!
-  adapter: node({
-    mode: "standalone"
-  })
+  output: 'server', 
+  adapter: cloudflare()
 });
